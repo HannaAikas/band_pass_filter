6f717bce41e1d73a3b21200e6dfee584ab947396
Hanna <hannaaikas@gmail.com>
2019-09-18 18:43:47 +0100

    replaces values below lower limit with lower limit

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -1,3 +1,9 @@
 def band_pass_filter(array, lower_limit, upper_limit)
-  array
+  array.map { |frequency|
+    if frequency < lower_limit
+      lower_limit
+    else
+      frequency
+    end
+   }
 end
--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -9,4 +9,8 @@ describe do
     result = band_pass_filter([90,90,100,110,110], 90, 110)
     expect(result).to eq([90,90,100,110,110])
   end
+  it "replaces values that are too low with lower limit value" do
+    result = band_pass_filter([80,90,100,110,110], 90, 110)
+    expect(result).to eq([90,90,100,110,110])
+  end
 end
