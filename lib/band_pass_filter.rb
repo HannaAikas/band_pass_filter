f06726e2fce813f2ca6004f5ffb8adbc28d06acc
Hanna <hannaaikas@gmail.com>
2019-09-18 18:46:33 +0100

    replaces values that are too high with upper limit value

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -2,6 +2,8 @@ def band_pass_filter(array, lower_limit, upper_limit)
   array.map { |frequency|
     if frequency < lower_limit
       lower_limit
+    elsif frequency > upper_limit
+      upper_limit
     else
       frequency
     end
